515559dbad473cca25a4fbb48b513fa1d6db3f20
BlaBlaBlazzz <jason20031215@gmail.com>
2024-01-14 16:21:03 +0800

    Update data sampling

--- data_scaling.py
+++ data_scaling.py
@@ -1,13 +1,15 @@
 import json
 import numpy as np
 import random
+import pandas as pd
+
 
 def read_json(path):
     with open(path) as f:
         data = json.load(f)    
     return data
 
-def data_scaling(data, config):
+def mask_scaling(data, config):
     for key in data:
         masks = []
         masks = data[key]["train"] + data[key]["val"] + data[key]["test"]
@@ -21,8 +23,40 @@ def write_json(data, path):
     with open(path, "w") as f:
         json.dump(data, f)
 
+def gen_mask_json():
+    path = './slap/masks.json'
+    mask = read_json(path)
+    mask_scaling(mask, [0.05, 0.05, 0.9])
+    write_json(mask, "slap_v2.json")
+    
 
-path = './slap/masks.json'
-data = read_json(path)
-data_scaling(data, [0.05, 0.05, 0.9])
-write_json(data, "slap_v2.json")
+def write_sampling_masks():
+    data_path = "./datasets/slap/y.csv"
+    mask_path = './datasets/slap/masks.json'
+    label = pd.read_csv(data_path)
+    masks = read_json(mask_path)
+    # sample
+    sampling(label, masks)
+    write_json(masks, "masks.json")
+
+def sampling(label, masks):
+    for key in masks:
+        ids = masks[key]["train"] + masks[key]["val"] + masks[key]["test"]
+        random.shuffle(ids)
+        # initialize class dictionary
+        class_dict = {k:[] for k in range(int(max(label["class"])) + 1)}
+        [class_dict[int(label.iloc[i])].append(i) for i in ids]
+
+        # sampling 10 ids in train, val progress
+        masks[key]["train"] = [item for i in range(int(max(label["class"])) + 1) for item in class_dict[i][:10]]
+        masks[key]["val"] = [item for i in range(int(max(label["class"])) + 1) for item in class_dict[i][10:20]]
+        masks[key]["test"] = [item for i in range(int(max(label["class"])) + 1) for item in class_dict[i][20:]]
+
+        # random shuffle
+        random.shuffle(masks[key]["train"])
+        random.shuffle(masks[key]["val"])
+        random.shuffle(masks[key]["test"])
+    # print(masks)
+
+write_sampling_masks()        
+
