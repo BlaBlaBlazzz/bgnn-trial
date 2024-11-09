bfae226614400ef89adc035cb6eec9929642c5b0
BlaBlaBlazzz <jason20031215@gmail.com>
2024-11-09 17:04:25 +0800

    [feat] support baseline and aggBGNN models

--- scripts/run_v2.py
+++ scripts/run_v2.py
@@ -13,6 +13,10 @@ from bgnn.models.trompt import trompt
 from bgnn.models.tabnet import tabnet
 from bgnn.models.tabtransformer import tabtransformer
 from bgnn.models.fttransformer import fttransformer
+from bgnn.models.aggBGNN import aggBGNN
+from bgnn.models.aggBGNN_dnf import aggBGNN_dnf
+from bgnn.models.aggBGNN_dg import aggBGNN_dg
+from bgnn.models.aggBGNN_v2 import aggBGNN_v2
 from bgnn.scripts.utils import NpEncoder
 from bgnn.models.Base import BaseModel
 
@@ -23,15 +27,18 @@ import datetime
 from pathlib import Path
 from collections import defaultdict as ddict
 
-
 import pandas as pd
 import networkx as nx
+import dgl
 import random
+import warnings
 import numpy as np
 import fire
 from omegaconf import OmegaConf
 from sklearn.model_selection import ParameterGrid
 import xgboost as xgb
+from catboost import CatBoostClassifier, CatBoostRegressor 
+from sklearn.metrics.pairwise import cosine_similarity
 
 class RunModel:
     def read_input(self, input_folder):
@@ -97,7 +104,8 @@ class RunModel:
         elif dataset == 'slap':
             input_folder = dataset_dir / 'slap'
         else:
-            input_folder = dataset_dir / f"{dataset}_s4"
+            input_folder = dataset_dir / f'{dataset}_s10'
+            print(input_folder)
 
         if self.save_folder is None:
             self.save_folder = f'results/{dataset}/{datetime.datetime.now().strftime("%d_%m")}'
@@ -109,6 +117,7 @@ class RunModel:
     def run_one_model(self, config_fn, model_name):
         self.config = OmegaConf.load(config_fn)
         grid = ParameterGrid(dict(self.config.hp))
+        
 
         for ps in grid:
             param_string = ''.join([f'-{key}{ps[key]}' for key in ps])
@@ -121,23 +130,21 @@ class RunModel:
             for _ in range(self.repeat_exp):
                 start = time.time()
                 model = self.define_model(model_name, ps)
-
+                
                 inputs = {'X': self.X, 'y': self.y, 'train_mask': self.train_mask,
                           'val_mask': self.val_mask, 'test_mask': self.test_mask, 'cat_features': self.cat_features}
                 # graph
                 if model_name in ['gnn', 'resgnn', 'resgnn_LI', 'bgnn', 'resgnnL', 'resgnnSVM', 'resgnnXG', 'bgnn_v2']:
-                    if self.networkx_graph:
-                        inputs['networkx_graph'] = self.networkx_graph
-
+                    inputs['networkx_graph'] = self.graph
+                elif model_name in ['aggBGNN', 'aggBGNN_dnf', 'aggBGNN_dg', 'aggBGNN_v2']:
+                    inputs['graph'] = self.graph
+                    inputs['graph_pred'] = self.graph_pred
+                    inputs['graph_leaf'] = self.graph_leaf
+                
                 metrics = model.fit(num_epochs=self.config.num_epochs, patience=self.config.patience,
                            loss_fn=f"{self.seed_folder}/{exp_name}.txt",
                            metric_name='loss' if self.task == 'regression' else 'accuracy', **inputs)
                 finish = time.time()
-
-                # emb-GBDT part
-                # if model_name in ['gnn']:
-                #     metrics = self.train_emb_gbdt(model)
-                # print(metrics)
 
                 best_loss = min(metrics['loss'], key=lambda x: x[1])
                 best_custom = max(metrics['r2' if self.task == 'regression' else 'accuracy'], key=lambda x: x[1])
@@ -149,24 +156,6 @@ class RunModel:
                                        np.mean(times),
                                        )
             # print("store result:", self.store_results)
-
-    def train_emb_gbdt(self, model):
-        x = model.pandas_to_torch(self.X)[0]
-        node_features = model.init_node_features(x, False)
-        graph = model.networkx_to_torch(self.networkx_graph)
-        # node embedding
-        node_embedding = model.model(graph, node_features).detach().cpu().numpy()
-        
-        gbdt = GBDTCatBoost(self.task)
-        X = self.X.copy()
-        metrics = gbdt.fit(X, self.y, self.train_mask, self.val_mask, self.test_mask,
-                          cat_features=self.cat_features,
-                          num_epochs=100, patience=100,
-                          plot=False, verbose=False, loss_fn=None,
-                          metric_name='loss' if self.task == 'regression' else 'accuracy',
-                          gnn_embedding = node_embedding)
-        return metrics
-        
     
     def define_model(self, model_name, ps):
         if model_name == 'catboost':
@@ -207,7 +196,6 @@ class RunModel:
                      num_epochs=100, patience=100,
                      plot=False, verbose=False, loss_fn=None,
                      metric_name='loss' if self.task == 'regression' else 'accuracy')
-            prediction = gbdt.model.predict(self.X)
             leaf_idx = gbdt.model.calc_leaf_indexes(self.X)
             # print("leaf_idx", leaf_idx)
             return GNN(task=self.task, gbdt_predictions=leaf_idx, **ps)
@@ -223,15 +211,16 @@ class RunModel:
             # print("prediction:", predictions.shape)
             return GNN(task=self.task, gbdt_predictions=predictions, **ps)
         elif model_name == 'resgnnXG':
+            x = self.X.copy()
             if self.cat_features is not None:
                 for col in list(self.X.columns[self.cat_features]):
-                    self.X[col] = self.X[col].astype('category')
+                    x[col] = self.X[col].astype('category')
             gbdt = GBDTXGBoost(self.task)
-            gbdt.fit(self.X, self.y, self.train_mask, self.val_mask, self.test_mask,
+            gbdt.fit(x, self.y, self.train_mask, self.val_mask, self.test_mask,
                      cat_features = self.cat_features,
                      num_epochs = 300, patience = 100,
-                     loss_fn=None, metric_name='accuracy')
-            dX = xgb.DMatrix(self.X, enable_categorical=True)
+                     loss_fn=None, metric_name='accuracy' if self.task=='classification' else 'loss')
+            dX = xgb.DMatrix(x, enable_categorical=True)
             predictions = gbdt.model.predict(dX)
             predictions = np.reshape(predictions, (len(predictions), 1))
             return GNN(task=self.task, gbdt_predictions=predictions, **ps)
@@ -260,6 +249,9 @@ class RunModel:
             json.dump(self.seed_results, f)
 
         self.aggregated = self.aggregate_results()
+        with open(f'{self.seed_folder}/aggregated_results.json', 'w+') as f:
+            json.dump(self.aggregated, f)
+
         with open(f'{self.save_folder}/aggregated_results.json', 'w+') as f:
             json.dump(self.aggregated, f)
 
@@ -286,7 +278,8 @@ class RunModel:
 
     def aggregate_results(self):
         algos = ['catboost', 'lightgbm', 'mlp', 'gnn', 'resgnn', 'resgnn_LI', 'bgnn', 'bgnn_v2', 'resgnnL', 'resgnnSVM', 'resgnnXG',
-                 'emb-GBDT', 'ExcelFormer', 'trompt', 'fttransformer', 'tabnet', 'tabtransformer']
+                 'emb-GBDT', 'ExcelFormer', 'trompt', 'fttransformer', 'tabnet', 'tabtransformer', 
+                 'aggBGNN', 'aggBGNN_dnf', 'aggBGNN_dg', 'aggBGNN_v2']
         model_best_score = ddict(list)
         model_best_time = ddict(list)
 
@@ -319,18 +312,79 @@ class RunModel:
                                  np.mean(model_best_time[model]), np.std(model_best_time[model]))
         return aggregated
     
+    def feature_vector(self):
+        if self.task == 'classification':
+            catboost_loss_function = 'MultiClass'
+            catboost_object = CatBoostClassifier
+        else:
+            catboost_loss_function = 'RMSE'
+            catboost_object = CatBoostRegressor
+
+        model = catboost_object(iterations=100,
+                                   depth=6,
+                                   learning_rate=0.1,
+                                   loss_function=catboost_loss_function,
+                                   random_seed=0,
+                                   nan_mode='Min',
+                                   allow_const_label=True)
+        
+        X_train = self.X.iloc[self.train_mask]
+        y_train = self.y.iloc[self.train_mask]
+
+        model.fit(X_train, y_train, verbose=False)
+        # prediction
+        if self.task == 'classification':
+            prediction = model.predict_proba(self.X)
+        else:
+            prediction = model.predict(self.X)
+        # leaf index
+        leaf_index = model.calc_leaf_indexes(self.X)
+        return prediction, leaf_index
+    
+    def construct_graph(self, nf):
+        warnings.filterwarnings('ignore')   # ignore dgl warnings
+        graph = dgl.DGLGraph()
+        if isinstance(nf, np.ndarray):
+            nf = pd.DataFrame(nf)
+        nodes = list(nf.index)
+        graph.add_nodes(len(nodes))
+
+        simul = cosine_similarity(nf.values, nf.values)
+        np.fill_diagonal(simul, 0)
+        top5 = np.argpartition(simul, -5)[:, -5:]
+
+        src_node = []
+        dst_node = []
+        for i in nodes:
+            for j in top5[i]:
+                src_node.append(i)
+                dst_node.append(j)
+        
+        graph.add_edges(src_node, dst_node)
+        graph = dgl.remove_self_loop(graph)
+        graph = dgl.add_self_loop(graph)
+
+        return graph
+    
     def get_graph(self, input_folder):
         if os.path.exists(f'{input_folder}/graph.graphml'):
             networkx_graph = nx.read_graphml(f'{input_folder}/graph.graphml')
             networkx_graph = nx.relabel_nodes(networkx_graph, {str(i): i for i in range(len(networkx_graph))})
             self.networkx_graph = networkx_graph
         elif os.path.exists(f'{input_folder}/cat_features.txt'):
+            # normalize
             encoded_X = BaseModel().encode_cat_features(self.X, self.y, self.cat_features, self.train_mask, 
                                                         self.val_mask, self.test_mask)
             encoded_X = BaseModel().normalize_features(encoded_X, self.train_mask, self.val_mask, self.test_mask)
-            self.networkx_graph = BaseModel().construct_graph(encoded_X)
+            self.graph = self.construct_graph(encoded_X)
         else:
-            self.networkx_graph = BaseModel().construct_graph(self.X)
+            self.graph = self.construct_graph(self.X)
+        
+        pred_matrix, leaf_index = self.feature_vector()
+        self.graph_pred = self.construct_graph(pred_matrix)
+        self.graph_leaf = self.construct_graph(leaf_index)
+
+        
 
     def run(self, dataset: str, *args,
             save_folder: str = None,
@@ -345,13 +399,13 @@ class RunModel:
         self.max_seeds = max_seeds
         print(dataset, args, task, repeat_exp, max_seeds, dataset_dir, config_dir)
 
-        dataset_dir = Path(dataset_dir) if dataset_dir else Path(__file__).parent.parent / 'datasets' / 'huggingFace_sd' / dataset
+        dataset_dir = Path(dataset_dir) if dataset_dir else Path(__file__).parent.parent / 'datasets' / 'huggingFace_sd_regression' / dataset
         config_dir = Path(config_dir) if config_dir else Path(__file__).parent.parent / 'configs' / 'model'
         print(dataset_dir, config_dir)
 
         self.task = task
         self.save_folder = save_folder
-        self.save_folder = Path('./results/huggingFace_sd') / dataset / f"{dataset}_s4" / save_folder
+        # self.save_folder = Path('./results/huggingFace_sd_regression') / dataset / f'{dataset}_s10' / save_folder
         self.get_input(dataset_dir, dataset)
 
         self.seed_results = dict()
@@ -363,12 +417,13 @@ class RunModel:
             self.create_save_folder(seed)
             self.split_masks(seed)
 
+            # construct graph
             self.get_graph(dataset_dir/dataset)
+            
 
             self.store_results = dict()
             for arg in args:
                 if arg == 'all':
-                    # self.run_one_model(config_fn=config_dir / 'catboost.yaml', model_name="catboost")
                     # self.run_one_model(config_fn=config_dir / 'mlp.yaml', model_name="mlp")
                     self.run_one_model(config_fn=config_dir / 'bgnn.yaml', model_name="bgnn")
                     self.run_one_model(config_fn=config_dir / 'bgnn_v2.yaml', model_name="bgnn_v2")
@@ -383,6 +438,10 @@ class RunModel:
                     self.run_one_model(config_fn=config_dir / 'tabnet.yaml', model_name='tabnet')
                     self.run_one_model(config_fn=config_dir / 'tabtransformer.yaml', model_name='tabtransformer')
                     self.run_one_model(config_fn=config_dir / 'fttransformer.yaml', model_name='fttransformer')
+                    self.run_one_model(config_fn=config_dir / 'aggBGNN.yaml', model_name="aggBGNN")
+                    self.run_one_model(config_fn=config_dir / 'aggBGNN_dnf.yaml', model_name="aggBGNN_dnf")
+                    self.run_one_model(config_fn=config_dir / 'aggBGNN_dg.yaml', model_name="aggBGNN_dg")
+                    self.run_one_model(config_fn=config_dir / 'aggBGNN_dg.yaml', model_name="aggBGNN_v2")
                     break
                 elif arg == 'catboost':
                     self.run_one_model(config_fn=config_dir / 'catboost.yaml', model_name="catboost")
@@ -421,6 +480,7 @@ class RunModel:
                     # except:
                     #     raise ValueError("Model not found.")
 
+            
             self.save_results(seed)
             if ix+1 >= max_seeds:
                 break
@@ -428,17 +488,14 @@ class RunModel:
         print(f'Finished {dataset}: {time.time() - start2run} sec.')
 
 if __name__ == '__main__':
-    # fire.Fire(RunModel().run)
+    fire.Fire(RunModel().run)
 
-    datasets_ls = ['Indian_Liver_Patient_Patient_Records_KFolds_5folds', 'Intersectional_Bias_Assessment_Testing_Data',
-                   'nki70_arff', 'Parkinson_Dataset_with_replicated_acoustic_features', 'Penguins_Classified', 'Pima_Indians_Diabetes',
-                   'Pima_Indians_Diabetes_with_null_values', 'Pokmon_Legendary_Data', 'QSAR_Bioconcentration_classes_dataset',
-                   'Quality_Assessment_of_Digital_Colposcopies', 'Real_Estate_DataSet', 'Startup_Success_Prediction', 
-                   'Store_Data_Performance', 'student_grade_pass_or_fail_prediction', 'Swiss_banknote_conterfeit_detection',
-                   'The_Estonia_Disaster_Passenger_List', 'tokyo1', 'User_Knowledge_Modeling', 'Z_Alizadeh_Sani']
+    # sd_path = Path(__file__).parent.parent / 'datasets' / 'huggingFace_sd_regression'
+    # datasets_ls = [dir for dir in os.listdir(sd_path) if os.path.isdir(os.path.join(sd_path, dir))]
+    # datasets_ls = sorted(datasets_ls)
+    # print(datasets_ls)
 
-
-    for dataset in datasets_ls:
-        RunModel().run(dataset, "all",
-                       save_folder="selected_models",
-                       task="classification")
+    # for dataset in datasets_ls:
+    #     RunModel().run(dataset, "all",
+    #                    save_folder="selected_models",
+    #                    task="regression")
